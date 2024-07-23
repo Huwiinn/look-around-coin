e5cd23a85613448dfe8e333cea15bbf62952d261
hwuiinn <gnldls8406@gmail.com>
2024-07-23 23:07:23 +0900

    Fix : .yml, fetchData 수정

--- fetchData.js
+++ fetchData.js
@@ -7,6 +7,18 @@ require("dotenv").config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+
+console.log("SUPABASE_URL:", supabaseUrl);
+console.log("SUPABASE_KEY:", supabaseKey);
+
+if (!supabaseUrl) {
+  throw new Error("supabaseUrl is required.");
+}
+
+if (!supabaseKey) {
+  throw new Error("supabaseKey is required.");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const dev = process.env.NODE_ENV !== "production";
@@ -22,10 +34,10 @@ app.prepare().then(() => {
     console.log("> Ready on http://localhost:3000");
 
     try {
-      // const response = await axios.get(
-      //   "http://localhost:3000/api/fetchCoinMarketData"
-      // );
-      coinsGlobalData();
+      const response = await axios.get(
+        "http://localhost:3000/api/fetchCoinMarketData"
+      );
+      console.log("response : ", response);
       console.log("API call successful:", response.data);
     } catch (error) {
       console.error("Error calling API:", error);
