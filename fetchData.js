0b6831f466f01c577431dd4e1ed47bcd9b7ebad9
hwuiinn <gnldls8406@gmail.com>
2024-07-23 23:32:51 +0900

    1

--- fetchData.js
+++ fetchData.js
@@ -1,30 +1,12 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-const axios = require("axios");
-require("dotenv").config();
+export const coinsGlobalAutoFetchData = async () => {
+  try {
+    const res = await axios.get("https://api.coinlore.net/api/global");
+    const resData = res.data;
 
-const app = next({ dev });
-const handle = app.getRequestHandler();
+    return resData;
+  } catch (err) {
+    console.log("err Message : ", err);
+  }
+};
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(3000, async (err) => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
-
-    try {
-      const response = await axios.get(
-        "http://localhost:3000/api/fetchCoinMarketData"
-      );
-      console.log("response : ", response);
-      console.log("API call successful:", response.data);
-    } catch (error) {
-      console.error("Error calling API:", error);
-    }
-
-    process.exit(0);
-  });
-});
+coinsGlobalAutoFetchData();
