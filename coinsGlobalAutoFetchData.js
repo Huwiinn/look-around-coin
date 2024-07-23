ccfe62afe6c4450a372e7b5e15e2ffbd52497e8b
hwuiinn <gnldls8406@gmail.com>
2024-07-23 23:50:19 +0900

    Fix : 매일 아침 9시에 coinsGlobalAutoFetchData 함수 실행

---
+++ coinsGlobalAutoFetchData.js
@@ -0,0 +1,12 @@
+const coinsGlobalAutoFetchData = async () => {
+  try {
+    const res = await axios.get("https://api.coinlore.net/api/global");
+    const resData = res.data;
+
+    return resData;
+  } catch (err) {
+    console.log("err Message : ", err);
+  }
+};
+
+coinsGlobalAutoFetchData();
