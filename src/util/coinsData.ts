c8b12d369920a09ea35adac82fa9d403e9d08ff6
hwuiinn <gnldls8406@gmail.com>
2024-07-27 23:16:35 +0900

    Fix : cron job 매 시간 정각마다 실행

--- src/util/coinsData.ts
+++ src/util/coinsData.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// supabase db에서 데이터를 불러오도록 수정해야함.
+// 현재 supabase db에는 코인시장 데이터밖에 없음.
+// 각 상위 10개 코인을 db에 삽입시켜야하는 작업이 필요함. (1시간 단위로 해보자);
+
 export const coinsData = async () => {
   try {
     const res = await axios.get("https://api.coinlore.net/api/tickers/");
